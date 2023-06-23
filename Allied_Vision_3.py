395a2208a448efa2b097b64a06e7733eb3902b40
MilesJoseph <Miles.Klingenberg@outlook.com>
2023-06-23 16:04:14 -0700

    new

--- Allied_Vision_3.py
+++ Allied_Vision_3.py
@@ -15,7 +15,7 @@ mounts = server.get_mount_points()
 
 # define the pipeline to record images ad attach it to the "stream1" endpoint
 vimbasrc_factory = GstRtspServer.RTSPMediaFactory()
-vimbasrc_factory.set_launch('vimbasrc camera=DEV_1AB22C023E6B settingsfile=/home/miles/Documents/CS_5.xml ! videoconvert ! x264enc speed-preset=ultrafast tune=zerolatency ! rtph264pay name=pay0')
+vimbasrc_factory.set_launch('vimbasrc camera=DEV_1AB22C023E6B settingsfile=/home/miles/AquaVision_Pollock/Aquavision_Pollock/CS_5.xml ! videoconvert ! x264enc speed-preset=ultrafast tune=zerolatency ! rtph264pay name=pay0')
 mounts.add_factory("/stream1", vimbasrc_factory)
 server.attach(None)
 
